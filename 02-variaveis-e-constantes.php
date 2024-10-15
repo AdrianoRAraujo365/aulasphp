b0c74d078aee576bea2f28dd80f2eb6e968648d4
Adriano Rodrigues de Araujo <adriano.rodrigues365@outlook.com>
2024-10-15 17:27:05 -0300

    Exercicio utilizando Bootstrap styles no PHP

--- 02-variaveis-e-constantes.php
+++ 02-variaveis-e-constantes.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Variaveis e Constantes</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
     <h1>Variaveis e Constantes</h1>
@@ -38,9 +39,17 @@
     ?>
 
     <h2>Saida de dados simplificada/direta</h2>
-    <p>Estamos no <?= $curso?>   em <?=$ano?>  .</p>
 
-    <p><?=AUTOR?> trabalha no empresa <?=$empresa?> .</p>
+    <p class="alert alert-success">
+        Estamos no <span class="text-bg-danger"><?= $curso?></span>
+        em 
+        <span class="text-bg-primary"><?=$ano?></span>  .</p>
+
+    <p><?=AUTOR?> trabalha no empresa <?=$empresa?>.</p>
     
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js
+"></script>
 </body>
 </html>

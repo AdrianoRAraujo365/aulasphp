dc1208a6a875c199ca76d25d1152a7c484e7aa75
Adriano Rodrigues de Araujo <adriano.rodrigues365@outlook.com>
2024-10-15 16:55:20 -0300

    Variaveis-e-constantes PHP

---
+++ 02-variaveis-e-constantes.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis e Constantes</title>
+</head>
+<body>
+    <h1>Variaveis e Constantes</h1>
+    <hr>
+    <h2>Declaraçao e saida usando echo</h2>
+
+    <?php
+    //variaveis
+    $curso = "Programador Web";
+    $ano = 2024;
+    $area = "back-End";
+
+    //Estamos no Programador Web em 2024.
+
+    //Saida interpolada (usando aspas duplas)
+    echo "<p>Estamos no $curso em $ano.</p>";
+
+    //Com aspas simples, PHP NAO interpreta variavel
+
+    echo '<p>Estamos no $curso em $ano.</p>';
+
+    //Constantes
+    define("AUTOR", "Adriano");// Tradicional, antiga
+    const EMPRESA = "CLOUD OF PANTS";//nova, moderna
+
+    //Fulano trabalha na empresa  CLOUD OF PANTS Tecnologia
+
+    //(Usando concatenação [ponto final])
+    echo "<p>".AUTOR. " Trabalha na empresa ".EMPRESA."</p>";
+
+
+    ?>
+    
+</body>
+</html>

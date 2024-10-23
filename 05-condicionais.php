f7d93176f1d1eb88a15535632425c70603036a01
Adriano Rodrigues de Araujo <adriano.rodrigues365@outlook.com>
2024-10-23 15:14:18 -0300

    Condicional Encadeada com swithc/case

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -18,8 +18,8 @@
         <?php
 
         $numero = 500;
-        if($numero >=100){
-             echo "<p> O valor da variavel é $numero</p>";
+        if ($numero >= 100) {
+            echo "<p> O valor da variavel é $numero</p>";
         }
 
         ?>
@@ -30,29 +30,69 @@
         /*Controle de Estoque */
 
         $produto = "Geladeira";
-        $qtdEmEstoque = 5; //o que temos no momento
+        $qtdEmEstoque = 0; //o que temos no momento
         $qtdCritica = 2; //minimo necessário
 
         ?>
 
-        <h3>Produto: <?$produto?></h3>
-        <p><b>Estoque:</b> <?=$qtdEmEstoque?></p>
+        <h3>Produto: <? $produto ?></h3>
+        <p><b>Estoque:</b> <?= $qtdEmEstoque ?></p>
         <hr>
 
         <?php
         /* Se a quantidade em estoque for abaixo da quantidade critica, o sistema deve avisar e pedir para repor.  */
 
-       
-
-        if(){
-
-        }else{
-             /* Caso contrário, simplesmente avisar que o estoque está normal.  */
 
 
+        if ($qtdEmEstoque < $qtdCritica) {
+            echo "<p class='alert alert-danger'>É necessário repor!</p>";
+        } else {
+            /* Caso contrário, simplesmente avisar que o estoque está normal.  */
+            echo "<p class='alert alert-success'>Estoque normal.</p>";
         }
 
         ?>
+        <hr>
+        <h2>Condicional ENCADEADA</h2>
+
+        <?php
+        /* Verificando o produto e determinando diferentes periodos de garantia (em anos). */
+
+        if ($produto == "Ultrabook") {
+            $garantia = 5;
+        } elseif ($produto == "Geladeira") {
+            $garantia = 3;
+        } elseif ($produto == "TV") {
+            $garantia = 2;
+        } else {
+            $garantia = 1;
+        }
+
+        ?>
+
+        <p><?= $produto ?> tem garantia de <b><?= $garantia ?> anos</b></p>
+
+        <h2>Condicional ENCADEADA usando switch/case</h2>
+
+        <?php
+        switch ($produto) {
+            case "Ultrabook":
+                $garantiaB = 5;
+                break;
+            case "Geladeira":
+                $garantiaB = 3;
+                break;
+            case "TV":
+                $garantiaB = 2;
+                break;
+            default:
+                $garantiaB = 1;
+                break;
+        }
+
+
+        ?>
+        <p> Garantia de <?= $garantiaB ?> anos. </p>
 
 
 
